8443dc1f843bc2b11e954283682b47f11bb6810a
Yin Ho CHONG <yinhochong@macbook-pro-de-yinho.home>
2018-07-24 23:43:03 +0200

    watch Apply Hero Background CSS

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,13 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Learn to design and code React Apps</h1>
-    <p>Complete courses about the best tools and design systems. Prototype nad build apps with React and Swift</p>
-    <Link to="/page-2/">Watch the Video here</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn to design and code React Apps</h1>
+        <p>Complete courses about the best tools and design systems. Prototype nad build apps with React and Swift</p>
+        <Link to="/page-2/">Watch the Video here</Link>
+      </div>
+    </div>
   </div>
 )
 
