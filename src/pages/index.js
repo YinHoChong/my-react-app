ffc91ad14a6e0f635b84b1f064db22529def524e
Yin Ho CHONG <yinhochong@macbook-pro-de-yinho.home>
2018-07-24 22:39:08 +0200

    watch button text

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Learn to design and code React Apps</h1>
+    <p>Complete courses about the best tools and design systems. Prototype nad build apps with React and Swift</p>
+    <Link to="/page-2/">Watch the Video here</Link>
+  </div>
+)
+
+export default IndexPage
